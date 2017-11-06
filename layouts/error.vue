df112e42c08ef52e3a71ebfa948275da9c4c1075
Sergey <hardslimlife@mail.ru>
2017-11-06 17:35:01 +0200

    error page

---
+++ layouts/error.vue
@@ -0,0 +1,37 @@
+<template>
+  <section class="found-page">
+    <div class="container">
+      <div class="found-container">
+        <div class="txt-404"><span>404</span>ошибка</div>
+        <div class="found-icon"><span class="icon-ico-dostavka-page-02"></span></div>Страница не найдена или не верно задан адрес
+        <br>
+        <router-link to='/' class="btn btn--brown waves-effect waves-light">
+          На главную
+        </router-link>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+
+export default {
+  name: 'nuxt-error',
+  props: ['error'],
+  head () {
+    return {
+      title: this.error.message || 'An error occured'
+    }
+  }
+}
+</script>
+
+<style>
+  .found-page {
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+  }
+</style>
